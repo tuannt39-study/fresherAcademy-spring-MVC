d220d9fe207e17d74424228cac277edfff5d27fe
tuannt39 <tuannt39@gmail.com>
2018-04-15 22:47:18 +0700

    ModelAndView and String in Controller

--- src/main/java/vn/its/controller/LoginController.java
+++ src/main/java/vn/its/controller/LoginController.java
@@ -1,8 +1,8 @@
 package vn.its.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import vn.its.model.Fresher;
 
@@ -10,9 +10,8 @@ import vn.its.model.Fresher;
 public class LoginController {
 
 	@RequestMapping(value = "login")
-	public ModelAndView showLogin() {
-		ModelAndView view = new ModelAndView("login");
-		view.addObject("fresher", new Fresher("tuan", "ntt12345"));
-		return view;
+	public String showLogin(Model model) {
+		model.addAttribute("fresher", new Fresher("tuan", "ntt12345"));
+		return "login";
 	}
 }
